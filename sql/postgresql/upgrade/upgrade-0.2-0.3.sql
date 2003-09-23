9c231e931c61ace70d5c15f46b295724a286b7a9
Lars Pind <lars@pinds.com>
2003-09-23 23:30:36 +0000

    Added define_function_args: bug #55

--- sql/postgresql/upgrade/upgrade-0.2-0.3.sql
+++ sql/postgresql/upgrade/upgrade-0.2-0.3.sql
@@ -7,3 +7,6 @@ update bulk_mail_messages set new_send_date = send_date;
 alter table bulk_mail_messages alter column new_send_date set not null;
 alter table bulk_mail_messages drop column send_date;
 alter table bulk_mail_messages rename column new_send_date to send_date;
+
+select define_function_args('bulk_mail__new','bulk_mail_id,package_id,send_date,date_format,status;pending,from_addr,subject,reply_to,extra_headers,message,query,creation_date;now(),creation_user,creation_ip,context_id');
+
